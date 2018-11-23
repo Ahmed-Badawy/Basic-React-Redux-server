cda5e35bd38b28d5c02684b37e946472adfd1f85
Ahmed-Badawy <me@ahmed-badawy.com>
2018-11-23 14:38:14 +0200

    added immutableRedux

---
+++ react/Tests&Learning/immutableRedux/MainComponent.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class MainComponent extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    async componentDidMount(){
+        console.log(this.myRef);
+        let myVal = await new Promise( resolve=>setTimeout(_=>resolve("Await Sync Done"), 2000) );
+        console.log(myVal);
+    }
+
+    handleChange(e){
+        e.preventDefault();
+        let value = e.target.value;
+        this.props.dispatch( {type:"ActionOne",data:{field:value}} );
+    }
+
+    render() {
+        return (
+            <div className='container'>
+                <h1>immutableRedux</h1>
+                <form>
+                    <input ref={input=>this.myRef=input} onChange={this.handleChange} value={this.props.field} />
+                </form>                
+            </div>
+        )
+    }
+}
+
+MainComponent.propTypes = {
+    // field: PropTypes.number.isRequired
+//custom validators
+    // field: function(props, propName, componentName){ if(props[propName]<5) throw new Error(`${propName} must be greater than 5`) }
+}
+
+export default connect((state, props) => {
+    return {
+        field: state.field,
+    }
+  })(MainComponent);
